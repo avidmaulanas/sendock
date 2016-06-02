9220fb8077954f8d12167eacafebfed35805f7e9
Agung Yuliaji <agung.yuliaji@gmail.com>
2016-06-02 11:40:45 +0700

    update service worker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,6 +34,7 @@ $(function(){
 })
 
 // if (window.location.hostname == "127.0.0.1"){
+if (window.location.protocol == "https:")
   if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js')
       .then(function(reg) {
